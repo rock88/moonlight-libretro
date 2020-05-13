6f4bb249b03b97c77a68c4fed0ced0754761c0eb
rock88 <rock88a@gmail.com>
2020-05-13 21:47:03 +0300

    Add few build scripts

---
+++ build_libXv_lakka_switch.sh
@@ -0,0 +1,49 @@
+#!/bin/sh
+
+git clone https://github.com/freedesktop/xorg-libXv.git libXv_tmp
+cd libXv_tmp
+
+# Fix TOOLCHAIN path!
+export TOOLCHAIN=/home/rock88/Documents/Lakka-LibreELEC/build.Lakka-L4T.aarch64-2.2-devel/toolchain
+
+export TARGET_CPU="cortex-a57"
+export TARGET_CPU_FLAGS="+crypto+crc+fp+simd"
+export TARGET_SUBARCH=aarch64
+export TARGET_ARCH=aarch64
+export TARGET_VARIANT=armv8-a
+export TARGET_ABI=eabi
+export SIMD_SUPPORT="yes"
+export TARGET_KERNEL_ARCH=arm64
+export TARGET_NAME=$TARGET_SUBARCH-libreelec-linux-gnu${TARGET_ABI}
+
+export SYSROOT_PREFIX=$TOOLCHAIN/$TARGET_NAME/sysroot
+export TARGET_PREFIX=$TOOLCHAIN/bin/$TARGET_NAME-
+
+export HOST_CC=$TOOLCHAIN/bin/host-gcc
+export HOST_CXX=$TOOLCHAIN/bin/host-g++
+
+export CC="${TARGET_PREFIX}gcc"
+export CXX="${TARGET_PREFIX}g++"
+export CPP="${TARGET_PREFIX}cpp"
+export LD="${TARGET_PREFIX}ld"
+export AS="${TARGET_PREFIX}as"
+export AR="${TARGET_PREFIX}ar"
+export NM="${TARGET_PREFIX}nm"
+export RANLIB="${TARGET_PREFIX}ranlib"
+
+export TARGET_CFLAGS="-mcpu=${TARGET_CPU}${TARGET_CPU_FLAGS} -mabi=lp64 -Wno-psabi -mtune=${TARGET_CPU} -march=${TARGET_VARIANT}${TARGET_CPU_FLAGS}"
+export TARGET_LDFLAGS="-mcpu=${TARGET_CPU}${TARGET_CPU_FLAGS}"
+export GCC_OPTS="--with-abi=lp64 --with-arch=$TARGET_VARIANT"
+
+export TARGET_CXXFLAGS="$TARGET_CFLAGS"
+export CPPFLAGS="$TARGET_CPPFLAGS"
+export CFLAGS="$TARGET_CFLAGS"
+export CXXFLAGS="$TARGET_CXXFLAGS"
+export LDFLAGS="$TARGET_LDFLAGS"
+
+export PKG_CONFIG_PATH=$SYSROOT_PREFIX/usr/lib/pkgconfig
+
+sh autogen.sh --prefix=$SYSROOT_PREFIX/usr --host=$TARGET_NAME --includedir=$SYSROOT_PREFIX/usr/include --disable-malloc0returnsnull
+make install
+cd ..
+rm -fr ffmpeg_tmp
